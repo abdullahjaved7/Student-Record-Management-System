6aa12f4171d9f48e4e43292bda8cac3de626b50d
Abdullah Javed <abdullahjaved@Abdullahs-MacBook-Air.local>
2026-06-15 16:05:39 +0200

    Added search student feature

--- Students_Record_Management_v1.py
+++ Students_Record_Management_v1.py
@@ -16,19 +16,35 @@ def add_student(students):
 
         students.append(student)
 
+        print("Student record added successfully!")
+
 
 def view_students(students):
+
     count = 1
+    print("  | Name    | Age | Grades |")
     for student in students:
         print(
             f"{count}. {student['name']} - {student['age']} - {student['grades']}")
         count += 1
 
 
+def search_student(students):
+    s_name = input("Enter Student Name: ")
+    for s in students:
+        if s["name"] == s_name:
+            print("STUDENT RECORD:")
+            print(f" {s['name']} - {s['age']} - {s['grades']}")
+            break
+        else:
+            print("Student Record Not Found.")
+
+
 while True:
     print("--- Student Record Management ---")
     print("1. Add Student Record.")
     print("2. View All Students Record.")
+    print("3. Search for a Student record.")
     print("0. Exit System.")
 
     choice = int(input("Enter Number(0 - 5): "))
@@ -39,6 +55,9 @@ while True:
 
         case 2:
             view_students(students)
+
+        case 3:
+            search_student(students)
 
         case 0:
             break
